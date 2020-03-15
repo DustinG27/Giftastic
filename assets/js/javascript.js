093a4d77be334a7c60b583874650e4807d6bf03f
dustin george <DustinG27@gmail.com>
2020-03-15 18:19:06 -0400

    added buttons for the gifs to be loaded

---
+++ assets/js/javascript.js
@@ -0,0 +1,80 @@
+$(document).ready(function() {
+  // create variables for search results and buttons
+
+  var topics = ["Rick Sanchez", "Morty", "Mr.Poopybutthole"];
+
+  // connect to gify through ajax
+
+  $("#submitPress").on("click", "btn", function() {
+    var dataName = $(this).attr("data-name");
+
+    var queryURL =
+      "https://api.giphy.com/v1/gifs/search?q=" +
+      dataName +
+      "&api_key=bAO2QEf1si5vORdg2P2ONcJAe9QMbxT9&limit=10";
+
+      console.log(dataName);
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+
+      var results = response.data;
+
+      for (var i = 0; i < results.length; i++) {
+        var rickDiv = $("<div>");
+
+        var p = $("<p>").text("Rating: " + results[i].rating);
+
+        var rickImage = $("<img>");
+
+        rickImage.attr("src", results[i].images.fixed_height.url);
+
+        // Appending the paragraph and image tag to the animalDiv
+        rickDiv.append(p);
+        rickDiv.append(rickImage);
+
+        // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+        $("#gifArea").prepend(rickDiv);
+      }
+    });
+
+    console.log(queryURL);
+  });
+
+  // log and display user input and push into the array
+
+  $("#submitPress").on("click", function(event) {
+    event.preventDefault();
+
+    var character = $("#form")
+      .val()
+      .trim();
+
+    topics.push(character);
+    $("#user-input").val("");
+
+    displayBtn();
+
+    console.log(topics);
+  });
+
+
+    //function to create the buttons from the array
+
+    function displayBtn() {
+        $("#buttonArea").empty();
+    
+        for (var i = 0; i < topics.length; i++) {
+          var a = $("<button>");
+          a.addClass("btn btn-default");
+          a.attr("data-name", topics[i]);
+          a.text(topics[i]);
+          $("#buttonArea").append(a);
+        }
+      }
+      displayBtn();
+    
+});
